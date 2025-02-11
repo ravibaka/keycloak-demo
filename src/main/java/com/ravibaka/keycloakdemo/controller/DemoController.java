85ab22adc8968df0fa0cf1b6cefdf32dc1892c6e
Ravi Baka <bakaravi.c@gmail.com>
2025-02-11 23:51:50 +0100

    Creating springboot keycloak demo application

---
+++ src/main/java/com/ravibaka/keycloakdemo/controller/DemoController.java
@@ -0,0 +1,30 @@
+package com.ravibaka.keycloakdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class DemoController {
+
+    @GetMapping("/hello-1")
+    public String hello1() {
+        return "Anybody can access this API";
+    }
+
+    @GetMapping("/hello-2")
+    public String hello2() {
+        return "Only Admin can access this API";
+    }
+
+    @GetMapping("/hello-3")
+    public String hello3() {
+        return "Only User can access this API";
+    }
+
+    @GetMapping("/hello-4")
+    public String hello4() {
+        return "Admin/User can access this API";
+    }
+}
